ced0b739519f9c52177dd322456ce30725c8e394
Rijan Subedi <rijansubedi0@gmail.com>
2025-11-16 07:41:56 +0545

    update tags

--- resources/views/layouts/alert.blade.php
+++ resources/views/layouts/alert.blade.php
@@ -1,22 +1,34 @@
-@if(Session::has('success'))
-<div class="fixed top-6 right-6 flex items-center p-4 rounded-xl shadow-2xl bg-white border border-blue-200 z-50 transform transition-all duration-300 hover:scale-[1.02]" id="alert">
+@if (Session::has('success'))
+    <div class="fixed top-6 right-6 flex items-center p-4 rounded-xl shadow-2xl bg-white border border-blue-200 z-50 transform transition-all duration-300 hover:scale-[1.02]"
+        id="alert">
 
 
-    <p class="text-base font-medium text-green-500">
-        {{Session('success')}}
-    </p>
-    <script>
-        //hide alert after 3 seconds with slide in and slide out effect
-        setTimeout(function(){
-            var alert = document.getElementById('alert');
-            alert.style.transition = 'all 0.5s ease-in-out';
-            alert.style.transform = 'translateY(-100%)';
-            alert.style.opacity = '0';
-            setTimeout(function(){
-                alert.remove();
-            }, 500);
-        }, 3000);
-    </script>
-
-</div>
+        <p class="text-base font-medium text-green-500">
+            {{ Session('success') }}
+        </p>
+    </div>
 @endif
+
+@if ($errors->any())
+    <div class="fixed top-6 right-6 flex items-center p-4 rounded-xl shadow-2xl bg-white border border-red-200 z-50 transform transition-all duration-300 hover:scale-[1.02]"
+        id="alert">
+        @foreach ($errors->all() as $error)
+            <p class="text-base font-medium text-red-500">
+                {{ $error }}
+            </p>
+        @endforeach
+    </div>
+
+@endif
+<script>
+    //hide alert after 3 seconds with slide in and slide out effect
+    setTimeout(function() {
+        var alert = document.getElementById('alert');
+        alert.style.transition = 'all 0.5s ease-in-out';
+        alert.style.transform = 'translateY(-100%)';
+        alert.style.opacity = '0';
+        setTimeout(function() {
+            alert.remove();
+        }, 500);
+    }, 3000);
+</script>
--- resources/views/tags/index.blade.php
+++ resources/views/tags/index.blade.php
@@ -17,7 +17,7 @@
         <td class="p-2 border ">{{$loop->iteration}}</td>
         <td class="p-2 border ">{{$tag->name}}</td>
         <td class="p-2 border ">
-            <a href="" class="bg-blue-600 text-white px-4 py-1 rounded mr-2">Edit</a>
+            <a onclick="showEditModal('{{$tag->id}}','{{$tag->name}}')"  class="bg-blue-600 text-white px-4 py-1 rounded mr-2 cursor-pointer">Edit</a>
             <a href="" class="bg-red-600 text-white px-4 py-1 rounded mr-2">Delete</a>
     </tr>
     @endforeach
@@ -35,6 +35,17 @@
         </div>
     </form>
 </div>
+<div id="editmodal" class="hidden fixed inset-0 bg-blue-500 bg-opacity-25 backdrop-blur-sm items-center justify-center">
+    <form action="" method="POST" class="p-4 bg-white rounded-lg w-1/2">
+        @csrf
+        <h1 class="font-bold text-xl">Edit TAG</h1>
+        <input type="text" id="name" name="name" placeholder="Enter Tag" class="block w-full rounded ">
+        <div class="flex justify-end mt-4">
+            <input onclick="hideEditModal()" type="button" class="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600 mr-2" value="Cancel">
+            <input type="submit" class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700" value="Edit">
+        </div>
+    </form>
+</div>
 
 <script>
     function showModal(){
@@ -44,6 +55,17 @@
     function hideModal(){
         document.getElementById('modal').classList.remove('flex');
         document.getElementById('modal').classList.add('hidden');
+    }
+</script>
+<script>
+    function showEditModal(id,name){
+        document.getElementById('editmodal').classList.remove('hidden');
+        document.getElementById('editmodal').classList.add('flex');
+        document.getElementById('name').value=name;
+    }
+    function hideEditModal(){
+        document.getElementById('editmodal').classList.remove('flex');
+        document.getElementById('editmodal').classList.add('hidden');
     }
 </script>
 @endsection
